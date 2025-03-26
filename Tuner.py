2ac01d866882a712529ce0698695f74df9fe3d47
Cockatoo <florian.jeunet@cpe.fr>
2025-03-26 10:54:57 +0100

    TunerGraphic

--- Tuner.py
+++ Tuner.py
@@ -3,6 +3,10 @@ from numpy.fft import fft
 from numpy import argmax, log2
 import numpy as np
 
+import PyQt5.QtWidgets
+from PyQt5.QtWidgets import QLabel, QVBoxLayout,QWidget
+
+
 notes  = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#",]
 
 rate, signal = read("ProtestMonoBruitTronque.wav")
@@ -27,3 +31,9 @@ print(round(res)/12)"""
 print("min : ", int(2**((round(res) -0.1)/12)*440), "note: ", notes[round(res)], " max: ", int(2**((round(res) +0.1)/12)*440 + 3))
 
 
+window = QWidget()
+layout = QVBoxLayout()
+labelNote = QLabel(layout)
+renderArea = QWidget(layout)
+renderArea.setPixmap
+window.setLayout(layout)
