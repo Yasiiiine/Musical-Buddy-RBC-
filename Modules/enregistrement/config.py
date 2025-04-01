b5bd3e8782ee35faf9b06c6b8f5a78d3348ac440
Yassine <yassine.belbashir@gmail.com>
2025-04-01 13:47:47 +0200

    Enregistrement

---
+++ Modules/enregistrement/config.py
@@ -0,0 +1,11 @@
+# Dossier de sortie pour les enregistrements
+OUTPUT_DIR = "Assets/recordings/"
+
+# Couleurs de l'affichage
+BG_COLOR = "#ffe6e6"
+
+# Paramètres audio
+RATE = 44100  # Hz
+CHANNELS = 1
+CHUNK = 1024
+FORMAT = "int16"
