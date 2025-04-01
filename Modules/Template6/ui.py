8ff1769efb48349c43f4b7cace6f18db9443c66d
med mahf <med.mahf.as@gmail.com>
2025-04-01 17:05:12 +0200

    Implementation module 6

--- Modules/Template6/ui.py
+++ Modules/Template6/ui.py
@@ -1,19 +1,30 @@
-# ui.py
-
 from PyQt5.QtWidgets import QWidget, QLabel, QVBoxLayout
 from PyQt5.QtCore import Qt
-
+from Modules.Template6.logic import MusicPlayer
 import Modules.Template6.config as cfg
+
 class Module6Screen(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.player = MusicPlayer()
+        self.label = QLabel("Appuyez sur la touche E pour jouer de la musique")
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setStyleSheet("font-size: 32px; font-weight: bold;")
+
         layout = QVBoxLayout()
-
-        label = QLabel(cfg.MODULE_LABEL)
-        label.setAlignment(Qt.AlignCenter)
-        label.setStyleSheet("font-size: 32px; font-weight: bold;")
-
-        layout.addWidget(label)
+        layout.addWidget(self.label)
         self.setLayout(layout)
         self.setStyleSheet(f"background-color: {cfg.MODULE_COLOR};")
+
+        self.setFocusPolicy(Qt.StrongFocus)  # Important pour capter les touches
+        self.setFocus()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_E:
+            if not self.player.is_playing():
+                self.player.play("Staying Alive")
+                self.label.setText("🎵 Lecture de la musique... (E pour stopper)")
+            else:
+                self.player.stop()
+                self.label.setText("Appuyez sur la touche E pour jouer de la musique")
